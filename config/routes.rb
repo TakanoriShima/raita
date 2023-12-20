cf5c472bdc521cfdda70cf9ae8988955f72149cd
Ryota.Fujitaka <ryouta.fujitaka@icloud.com>
2023-12-20 05:53:33 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
 
-  get 'signup', to: 'users#new'
+  # ログイン関連のルーティング
+  get 'login', to: 'sessions#new'    # ログインページを表示するためのGETリクエスト
+  post 'login', to: 'sessions#create' # ログイン処理を行うためのPOSTリクエスト
+  get 'logout', to: 'sessions#destroy' # ログアウト処理を行うためのGETリクエスト
+
+  # サインアップ関連のルーティング
+  get 'signup', to: 'users#new'  # サインアップページを表示するためのGETリクエスト
+  
   resources :users
 end
